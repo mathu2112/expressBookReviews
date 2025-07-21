234d528a85435f45643d672347ec8c2a03cd3e25
mathu2112 <mathushawlika1221@gmail.com>
2025-07-21 13:42:18 +0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+if (!req.session.authorization) {
+    return res.status(403).json({ message: "No authorization header found" });
+  }
+
+  const token = req.session.authorization.accessToken;
+
+  jwt.verify(token, 'secret_key', (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });//Write the authenication mechanism here
 });
  
 const PORT =5000;
